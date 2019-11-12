4f5e08f3edf889f7ba2b132ff503664e4959d053
Otshelnik-Fm <Otshelnik-Fm@yandex.ru>
2019-11-12 14:10:38 +0400

    v1.0

--- templates/gsb-share.php
+++ templates/gsb-share.php
@@ -24,36 +24,36 @@ $pin_url = 'https://pinterest.com/pin/create/button/?url=' . $link;
 ?>
 
 <div class="gsb_share_box">
-    <a class="gsb_ico gsb_fb" title="Facebook" onclick="window.open( '<?php echo $fb_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' );yaCounter46868760.reachGoal( 'fb_sh' ); return true;" href="javascript: void(0)" rel="nofollow noopener">
+    <a class="gsb_ico gsb_fb" title="Facebook" onclick="window.open( '<?php echo $fb_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' ); return true;" href="javascript: void(0)" rel="nofollow noopener" aria-label="Поделиться в Facebook">
         <i class="rcli fa-facebook" aria-hidden="true"></i>
     </a>
 
-    <a class="gsb_ico gsb_tw" title="Twitter" onClick="window.open( '<?php echo $tw_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' );yaCounter46868760.reachGoal( 'tw_sh' );return true;" href="javascript: void(0)" rel="nofollow noopener">
+    <a class="gsb_ico gsb_tw" title="Twitter" onClick="window.open( '<?php echo $tw_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' ); return true;" href="javascript: void(0)" rel="nofollow noopener" aria-label="Поделиться в Twitter">
         <i class="rcli fa-twitter" aria-hidden="true"></i>
     </a>
 
-    <a class="gsb_ico gsb_vk" title="ВКонтакте" onClick="window.open( '<?php echo $vk_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' );yaCounter46868760.reachGoal( 'vk_sh' );return true;" href="javascript: void(0)" rel="nofollow noopener">
+    <a class="gsb_ico gsb_vk" title="ВКонтакте" onClick="window.open( '<?php echo $vk_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' ); return true;" href="javascript: void(0)" rel="nofollow noopener" aria-label="Поделиться в ВКонтакте">
         <i class="rcli fa-vk" aria-hidden="true"></i>
     </a>
 
-    <a class="gsb_ico gsb_tg no_marked_icon" title="Telegram" href="javascript:window.open('https://t.me/share/url?url='+encodeURIComponent(window.location.href), '_blank')" rel="nofollow noopener">
+    <a class="gsb_ico gsb_tg no_marked_icon" title="Telegram" href="javascript:window.open('https://t.me/share/url?url='+encodeURIComponent(window.location.href), '_blank')" rel="nofollow noopener" aria-label="Поделиться в Telegram">
         <i class="rcli fa-paper-plane" aria-hidden="true"></i>
     </a>
 
-    <a class="gsb_ico gsb_ok" title="Одноклассники" onClick="window.open( '<?php echo $ok_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' );yaCounter46868760.reachGoal( 'ok_sh' );return true;" href="javascript: void(0)" rel="nofollow noopener">
+    <a class="gsb_ico gsb_ok" title="Одноклассники" onClick="window.open( '<?php echo $ok_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' ); return true;" href="javascript: void(0)" rel="nofollow noopener" aria-label="Поделиться в Одноклассники">
         <i class="rcli fa-odnoklassniki" aria-hidden="true"></i>
     </a>
 
-    <a class="gsb_ico gsb_mr" title="Мой Мир" onClick="window.open( '<?php echo $mr_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' );yaCounter46868760.reachGoal( 'mr_sh' );return true;" href="javascript: void(0)" rel="nofollow noopener">
+    <a class="gsb_ico gsb_mr" title="Мой Мир" onClick="window.open( '<?php echo $mr_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' ); return true;" href="javascript: void(0)" rel="nofollow noopener" aria-label="Поделиться в Мой Мир">
         <i class="rcli fa-at" aria-hidden="true"></i>
     </a>
 
-    <a class="gsb_ico gsb_pin" title="Pinterest" onClick="window.open( '<?php echo $pin_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' );yaCounter46868760.reachGoal( 'pin_sh' );return true;" href="javascript: void(0)" rel="nofollow noopener">
+    <a class="gsb_ico gsb_pin" title="Pinterest" onClick="window.open( '<?php echo $pin_url; ?>', 'sharer', 'toolbar=0,status=0,width=700,height=400' ); return true;" href="javascript: void(0)" rel="nofollow noopener" aria-label="Поделиться в Pinterest">
         <i class="rcli fa-pinterest" aria-hidden="true"></i>
     </a>
 
     <div class="gsb_other">
-        <a class="gsb_copy" title="Скопировать ссылку в буфер" href="javascript: void(0)" onclick="gsbCopy( this );return false;" rel="nofollow noopener">
+        <a class="gsb_copy" title="Скопировать ссылку в буфер" href="javascript: void(0)" onclick="gsbCopy( this );return false;" rel="nofollow noopener" aria-label="Скопировать ссылку в буфер">
             <i class="rcli fa-files-o" aria-hidden="true" title="Копировать ссылку"></i>
             <span>Копировать ссылку</span>
         </a>
