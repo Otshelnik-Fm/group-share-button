fbeab79a9eed224e347ede189167ad32424c9b6f
Otshelnik-Fm <Otshelnik-Fm@yandex.ru>
2020-01-30 18:17:53 +0400

    v1.0.2

--- assets/js/gsb-script.js
+++ assets/js/gsb-script.js
@@ -1,6 +1,7 @@
+// copy
 function gsbCopy( e ) {
     jQuery( e ).siblings( 'input.gsb_link_copy' ).select();
-    document.execCommand( "copy" );
+    document.execCommand( 'copy' );
     jQuery( e ).after( '<div class="gsb_mess">Скопировано</div>' ).fadeIn().delay( 1500 );
     jQuery( '.gsb_mess' ).delay( 2000 ).fadeOut();
 }
